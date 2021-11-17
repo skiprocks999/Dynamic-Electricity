84aaa9585a374ee98efa3c5a89195fd66a28fe12
skip999 <skiprocks999@gmail.com>
2021-11-16 19:59:06 -0600

    Fix tags not registering

--- src/main/java/dynamicelectricity/DynamicElectricity.java
+++ src/main/java/dynamicelectricity/DynamicElectricity.java
@@ -8,6 +8,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -15,6 +16,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import dynamicelectricity.client.ClientRegister;
+import dynamicelectricity.common.tags.DynamicElectricityTags;
 
 
 @Mod(References.ID)
@@ -32,6 +34,11 @@ public class DynamicElectricity
     	DeferredRegisters.CONTAINERS.register(bus);
     	DeferredRegisters.FLUIDS.register(bus);
     	SoundRegister.SOUNDS.register(bus);
+    }
+    
+    @SubscribeEvent
+    public static void onCommonSetup(FMLCommonSetupEvent event) {
+    	DynamicElectricityTags.init();
     }
     
     @SubscribeEvent
