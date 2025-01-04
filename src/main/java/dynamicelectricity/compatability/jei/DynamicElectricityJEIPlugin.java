623e9f896a58de50ca565742d4d7f5c4932f66d4
skip999 <skiprocks999@gmail.com>
2025-01-04 10:47:39 -0600

    add fluids to JEI menu

--- src/main/java/dynamicelectricity/compatability/jei/DynamicElectricityJEIPlugin.java
+++ src/main/java/dynamicelectricity/compatability/jei/DynamicElectricityJEIPlugin.java
@@ -1,9 +1,18 @@
 package dynamicelectricity.compatability.jei;
 
 import dynamicelectricity.References;
+import dynamicelectricity.registry.DynamicElectricityFluids;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.neoforge.NeoForgeTypes;
+import mezz.jei.api.registration.IExtraIngredientRegistration;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.material.Fluid;
+import net.neoforged.neoforge.fluids.FluidStack;
+import net.neoforged.neoforge.registries.DeferredHolder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @JeiPlugin
 public class DynamicElectricityJEIPlugin implements IModPlugin {
@@ -11,6 +20,15 @@ public class DynamicElectricityJEIPlugin implements IModPlugin {
 	@Override
 	public ResourceLocation getPluginUid() {
 		return ResourceLocation.fromNamespaceAndPath(References.ID, "jei");
+	}
+
+	@Override
+	public void registerExtraIngredients(IExtraIngredientRegistration registration) {
+		List<FluidStack> fluids = new ArrayList<>();
+		for (DeferredHolder<Fluid, ? extends Fluid> fluid : DynamicElectricityFluids.FLUIDS.getEntries()) {
+			fluids.add(new FluidStack(fluid.get(), 1000));
+		}
+		registration.addExtraIngredients(NeoForgeTypes.FLUID_STACK, fluids);
 	}
 
 }
