f300147bc7fb7244f39d47804a890f7a460fb0b8
skip999 <skiprocks999@gmail.com>
2025-07-20 14:31:52 -0500

    bug fixes

--- src/main/java/dynamicelectricity/common/inventory/container/ContainerMotorAC.java
+++ src/main/java/dynamicelectricity/common/inventory/container/ContainerMotorAC.java
@@ -13,7 +13,7 @@ import voltaic.prefab.inventory.container.types.GenericContainerBlockEntity;
 public class ContainerMotorAC extends GenericContainerBlockEntity<TileMotorAC> {
 	
 	public ContainerMotorAC(int id, Inventory playerinv) {
-		this(id, playerinv, new SimpleContainer(1), new SimpleContainerData(3));
+		this(id, playerinv, new SimpleContainer(1), new SimpleContainerData(5));
     }
 	
 	public ContainerMotorAC(int id, Inventory playerinv, Container inventory, ContainerData inventorydata) {
--- src/main/java/dynamicelectricity/common/inventory/container/ContainerMotorDC.java
+++ src/main/java/dynamicelectricity/common/inventory/container/ContainerMotorDC.java
@@ -16,7 +16,7 @@ import voltaic.prefab.utilities.math.Color;
 public class ContainerMotorDC extends GenericContainerBlockEntity<TileMotorDC> {
 
 	public ContainerMotorDC(int id, Inventory playerinv) {
-		this(id, playerinv, new SimpleContainer(2), new SimpleContainerData(3));
+		this(id, playerinv, new SimpleContainer(2), new SimpleContainerData(5));
     }
 	
 	public ContainerMotorDC(int id, Inventory playerinv, Container inventory, ContainerData inventorydata) {
